e49071c115c0bba4ba5c2ca447dfd8157fe191f9
Zeyad Hashem <Zeyadhashem188@gmail.com>
2024-02-27 00:00:13 +0200

    adding unit test and the changing user feature of django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email','name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('important_dates'), {'fields':('last_login',)})
+    )
+    readonly_fields = ['last_login',]
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -30,4 +30,11 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
-        self.assertContains(res, self.user.email )
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
