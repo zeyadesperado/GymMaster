376796986290d7d6d7a41a393794d696ad55ab31
Zeyad Hashem <Zeyadhashem188@gmail.com>
2024-02-28 21:10:32 +0200

    adding updating and retrieving end point for user, added authentication too

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,10 @@
 """
 Serializers for the user API view.
 """
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+    )
 from django.utils.translation import gettext as _
 
 from rest_framework import serializers
@@ -18,6 +21,17 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
