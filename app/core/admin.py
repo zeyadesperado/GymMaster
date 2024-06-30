3aa718ee6de89b0eca00c1a824d15496dcee03b2
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2024-07-01 01:28:54 +0300

    models amins in models.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -42,6 +42,38 @@ class UserAdmin(BaseUserAdmin):
         }],
     )
 
+class RecipeAdmin(admin.ModelAdmin):
+    """Define the admin pages for recipes."""
+    list_display = ['id', 'title', 'user', 'time_minutes', 'calories', 'price']
+    list_filter = ['user', 'tags']
+    search_fields = ['title', 'user__email']
+
+class TagAdmin(admin.ModelAdmin):
+    """Define the admin pages for tags."""
+    list_display = ['id', 'name', 'user']
+    search_fields = ['name', 'user__email']
+
+class IngredientAdmin(admin.ModelAdmin):
+    """Define the admin pages for ingredients."""
+    list_display = ['id', 'name', 'user']
+    search_fields = ['name', 'user__email']
+
+class SupplementAdmin(admin.ModelAdmin):
+    """Define the admin pages for supplements."""
+    list_display = ['id', 'name', 'price', 'calories']
+    search_fields = ['name']
+
+class PaymentAdmin(admin.ModelAdmin):
+    """Define the admin pages for payments."""
+    list_display = ['id', 'user', 'duration', 'price', 'created_at']
+    list_filter = ['user']
+    search_fields = ['user__email', 'user__id']
+
+class CoachAdmin(admin.ModelAdmin):
+    """Define the admin pages for coaches."""
+    list_display = ['id', 'name', 'price_per_month']
+    search_fields = ['name']
+
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Recipe)
