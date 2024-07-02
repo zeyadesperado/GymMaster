fdccc0169f09a100d12e3562681c6f22a3f5bf0a
Zeyad Hashem <zeyadhashem188@gmail.com>
2024-07-02 07:50:34 +0300

    removing shop

--- app/app/settings.py
+++ app/app/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'user',
     'recipe',
-    'shop',
+    # 'shop',
     'corsheaders',
 
 ]
--- app/shop/admin.py
+++ app/shop/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from shop import models
-
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'order_status', 'total_quantity', 'total_price', 'user']
-    list_filter = ['order_status', 'user']
-    search_fields = ['user__email', 'id']
-
-admin.site.register(models.Order, OrderAdmin)
+# Register your models here.
+admin.site.register(models.Product)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
